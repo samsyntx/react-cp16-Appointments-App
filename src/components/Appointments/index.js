c28f6a48b304e81f19d0029c91ccb0d665bd2f51
Shubham <samaroraytb@gmail.com>
2023-03-06 07:30:22 +0000

    React Appointments App

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,58 @@
+import {Component} from 'react'
+import './index.css'
+
+class Appointments extends Component {
+  render() {
+    return (
+      <div className="complete-bg-main-container">
+        <div className="white-bg-content-container">
+          <section className="top-section-container">
+            <h1 className="add-appointment-form-heading">Add Appointment</h1>
+            <div className="top-image-and-form-section">
+              <form className="complete-form-section">
+                <label className="label-element-style" htmlFor="titleInput">
+                  Title
+                </label>
+                <input
+                  className="input-element-style"
+                  type="text"
+                  id="titleInput"
+                  placeholder="Title"
+                />
+                <label className="label-element-style" htmlFor="dateInput">
+                  Date
+                </label>
+                <input
+                  className="input-element-style"
+                  type="date"
+                  id="dateInput"
+                />
+                <button className="add-event-button" type="submit">
+                  Add
+                </button>
+              </form>
+              <img
+                className="form-image"
+                src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
+                alt="appointments"
+              />
+            </div>
+          </section>
+          <hr />
+          <section className="bottom-section-list">
+            <div className="appointments-starts-head">
+              <h1 className="appointment-heading">Appointments</h1>
+              <button className="show-started-button" type="button">
+                Started
+              </button>
+            </div>
+            <ul className="unorder-appointment-list">
+              <h1>Hi</h1>
+            </ul>
+          </section>
+        </div>
+      </div>
+    )
+  }
+}
+export default Appointments
